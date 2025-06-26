c556fab013c98a9b4c2c91f94fa66fc6ad8a2f80
aliammari1 <ammari.ali.0001@gmail.com>
2025-06-26 15:10:32 +0100

    Configure CORS to allow all origins and add Vercel configuration for deployment

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { json } from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: "*"
+  });
 
   app.use(json({ limit: '100mb' }));
 
