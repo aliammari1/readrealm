4b48055f51ea1dc9039deab6cb9cf424a57eea31
Ali Ammari <ammari.ali.0001@gmail.com>
2026-09-22 08:00:41 +0100

    test(api): mock injected collaborators in smoke tests

--- apps/api/src/user/user.service.spec.ts
+++ apps/api/src/user/user.service.spec.ts
@@ -1,18 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
-    }).compile();
-
-    service = module.get<UserService>(UserService);
-  });
-
-  it('should be defined', () => {
+  it('should be defined with a mocked user model', () => {
+    const service = new UserService({} as any);
     expect(service).toBeDefined();
   });
 });
